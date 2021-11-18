d3f8250a0d2a5756ed96f84d82f024ec9b9fcbee
Teuschl Nikolas <niklas.teuschl@hotmail.com>
2021-11-18 03:41:27 +0100

    Changed from py2exe to pyinstaller for cross-platform support

---
+++ build.py
@@ -0,0 +1,26 @@
+import PyInstaller.__main__ as PyInstaller
+import platform, os
+
+my_sys = platform.system()
+install_options = [
+    "klog.py", 
+    "--onefile", 
+    "--nowindow", 
+    "--noconfirm",
+    "-p.", 
+    "-nKLog"
+    ]
+
+if my_sys == "Linux" or my_sys == "Windows":
+    PyInstaller.run(install_options)
+elif my_sys == "Darwin":
+    install_options.append("--osx-bundle-identifier at.neosecundus.klog")
+    PyInstaller.run(install_options)
+else:
+    print(f"Not implemented: Build for os '{my_sys}' cannot be run!")
+    exit(1)
+
+try:
+    os.remove("./KLog.spec")
+except FileNotFoundError:
+    print("\033[33mNo Spec file to remove! Skipping optional cleanup...\033[0m")
